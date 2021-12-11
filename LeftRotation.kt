f1da4f4931bb5bc814a7b4219d339d22532fd202
Saurav Dwivedi <saurav.dwivedi@gmail.com>
2021-12-11 14:58:53 +0100

    Add files via upload

---
+++ LeftRotation.kt
@@ -0,0 +1,39 @@
+/** A left rotation operation on an array shifts each of the array's
+ *  elements 1 unit to the left. For example, if 2 left rotations are
+ *  performed on array [1,2,3,4,5], then the array would become
+ *  [3,4,5,1,2]. Note that the lowest index item moves to the highest
+ *  index in a rotation. This is called a circular array.
+ *
+ *  Given an array a of n integers and a number, d, perform d left
+ *  rotations on the array. Return the updated array to be printed
+ *  as a single line of space-separated integers.
+ */
+
+
+fun oneLeftRot(a: MutableList<Int>): MutableList<Int> {
+    var x = mutableListOf<Int>()
+    for (j in 0 until a.size) {
+        if (j < a.size-1) {
+            x.add(a[j+1])
+        }
+        else {
+            x.add(a[0])
+        }
+    }
+return x
+}
+
+fun main() {
+    println("Enter array of integers (space separated e.g. '1 2 3'): ")
+    var a = readLine()!!.trim().split(" ").map{it.toInt()}.toMutableList()
+    println("Enter no. of rotations: ")
+    val d = readLine()!!.trimEnd().toInt()
+    var c = 0
+    while (c < d) {
+        var z = oneLeftRot(a)
+        c = c + 1
+        a = z
+    }
+    println("Array after $d rotations: [${a.joinToString(",")}]")
+}
+
