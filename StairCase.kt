f41fd8c23db0e4ce5479f6d91367c76b56446d0d
sauravdwivedi <saurav.dwivedi@gmail.com>
2022-04-24 12:00:54 +0200

    modify StairCase.kt

--- StairCase.kt
+++ StairCase.kt
@@ -10,25 +10,19 @@
  *  a staircase for a given integer n as input.
  */
 
-
 fun stairCase(stairCaseSize: Int): String {
     var stairCase: String = ""
-    
     for (i in 1 until stairCaseSize + 1) {
         for (j in 0 until stairCaseSize - x) {
            stairCase += " "
         }
-        
         for (k in 0 until x) {
            stairCase += "#"
         }
-        
         stairCase += "\n"
     }
-    
     return stairCase
 }
-
 
 fun main() {
     print("Size of staircase: ")
